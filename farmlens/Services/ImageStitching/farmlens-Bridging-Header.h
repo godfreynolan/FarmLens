fc305dc42e6d265d253d00adb00bbd6465cabbe9
Ian Timmis <itimmis@RIIS-MBPRO-09.local>
2018-02-15 09:04:57 -0500

    fixed bridging header for image stitching

--- farmlens/Services/ImageStitching/farmlens-Bridging-Header.h
+++ farmlens/Services/ImageStitching/farmlens-Bridging-Header.h
@@ -2,11 +2,4 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#ifndef CVOpenTemplate_Header_h
-#define CVOpenTemplate_Header_h
-
-//#include <opencv2/opencv.hpp>
-
-//cv::Mat stitch (std::vector <cv::Mat> & images);
-
-#endif
+#import "CVWrapper.h"
