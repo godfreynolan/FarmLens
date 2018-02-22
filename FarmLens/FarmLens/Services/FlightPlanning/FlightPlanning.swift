c8de543f44f881e91e2c96573f1f9db9e405d8ce
Tom Kocik <tkocikjr@comcast.net>
2018-02-22 13:29:00 -0500

    AGR-8: Fixed some compile issues. Started working on flight path

--- FarmLens/FarmLens/Services/FlightPlanning/FlightPlanning.swift
+++ FarmLens/FarmLens/Services/FlightPlanning/FlightPlanning.swift
@@ -10,16 +10,40 @@ import MapKit
 import UIKit
 
 class FlightPlanning {
+    private let flightDelta = 40 * 90 / 3280.4 / 10000
+    
     private var boundingArea: MKPolygon
     
     init(polygon: MKPolygon) {
         self.boundingArea = polygon
     }
     
-    func isCoordinateInBoundingArea(coordinate: CLLocationCoordinate2D) {
-        var renderer = MKPolygonRenderer(polygon: self.boundingArea)
-        var position = CGPointMake(coordinate.longitude, coordinate.latitude)
+    func isCoordinateInBoundingArea(coordinate: CLLocationCoordinate2D) -> Bool {
+        let renderer = MKPolygonRenderer(polygon: self.boundingArea)
+        let position = renderer.point(for: MKMapPoint(x: coordinate.longitude, y: coordinate.latitude))
         
         return renderer.path.contains(position)
+    }
+    
+    func createFlightPath(coordinateList: [CLLocationCoordinate2D]) {
+        let boundingRect = self.boundingArea.boundingMapRect
+        
+        let startPoint = boundingRect.origin
+        let endPoint = MKMapPointMake(MKMapRectGetMaxX(boundingRect), MKMapRectGetMaxY(boundingRect))
+        
+        var flightPath: [CLLocationCoordinate2D] = []
+        
+        var currentPoint = startPoint
+        while MKMapRectContainsPoint(boundingRect, currentPoint) {
+            flightPath.append(CLLocationCoordinate2D(latitude: currentPoint.y, longitude: currentPoint.x))
+            currentPoint.y = currentPoint.y + flightDelta
+        }
+    }
+    
+    private func increaseLatitude(boundingRect: MKMapRect, flightPath: [CLLocationCoordinate2D], currentPoint: MKMapPoint) {
+        while MKMapRectContainsPoint(boundingRect, currentPoint) {
+//            flightPath.append(CLLocationCoordinate2D(latitude: currentPoint.y, longitude: currentPoint.x))
+//            currentPoint.y = currentPoint.y + flightDelta
+        }
     }
 }
