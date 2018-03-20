8873521df9f5095d9b858c79084e4977923f696a
itimmisriis <30529318+itimmisriis@users.noreply.github.com>
2018-03-20 09:47:22 -0400

    Unit tested the image loader

---
+++ FarmLensTests/Services/Image/ImageLoaderTest.swift
@@ -0,0 +1,32 @@
+//
+//  ImageLoaderTest.swift
+//  FarmLensTests
+//
+//  Created by Ian Timmis on 3/20/18.
+//  Copyright © 2018 DJI. All rights reserved.
+//
+
+import XCTest
+@testable import FarmLens
+
+class ImageLoaderTest: XCTestCase {
+    
+    var loader:ImageLoader!
+    
+    override func setUp() {
+        super.setUp()
+        loader = ImageLoader()
+    }
+    
+    func testInit() {
+        XCTAssertNotNil(loader)
+    }
+    
+    func testLoadImagesNil() {
+        XCTAssert(loader.loadImages(imageCount: 0).count == 0)
+    }
+    
+    func testLoadImagesNotNil() {
+        XCTAssertNotNil(loader.loadImages(imageCount: 1))
+    }
+}
