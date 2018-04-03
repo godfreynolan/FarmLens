7daba1157471367a568d3aaac3a95405b00e845e
Tom Kocik <tkocikjr@comcast.net>
2018-04-03 14:56:18 -0400

    Revert "Delete images, grab x images for post processing"
    This reverts commit 612b7c4c4d3f3daaa10310d56078ff6ecf804572.

--- FarmLens/Services/Image/ImageLoader.swift
+++ FarmLens/Services/Image/ImageLoader.swift
@@ -32,7 +32,7 @@ class ImageLoader {
         imageOptions.isSynchronous = true
         imageOptions.resizeMode = .exact
         
-        for index in 0..<results.count {
+        for index in 0...results.count - 1 {
             let result = results[index]
             
             imageManager.requestImage(for: result, targetSize: CGSize(width: CGFloat(400), height: CGFloat(300)), contentMode: .aspectFill, options: imageOptions, resultHandler: { (uiImage, info) in
@@ -41,79 +41,5 @@ class ImageLoader {
         }
         
         return loadedImages
-    }
-    
-    func loadImagesForProcessing(batchSize: Int, imageCount: Int, iter: Int) -> [DroneImage] {
-        var loadedImages: [DroneImage] = []
-        
-        if imageCount == 0 {
-            return loadedImages
-        }
-        
-        let imageManager = PHImageManager.default()
-        let options = PHFetchOptions()
-        options.sortDescriptors = [
-            NSSortDescriptor(key:"creationDate", ascending: false)
-        ]
-        options.includeAssetSourceTypes = .typeUserLibrary
-        // Number of pre-processed images plus current post-processed images
-        options.fetchLimit = imageCount + batchSize * iter
-        options.includeAllBurstAssets = false
-        options.includeHiddenAssets = false
-        
-        let results = PHAsset.fetchAssets(with: .image, options: options)
-        
-        let imageOptions = PHImageRequestOptions()
-        imageOptions.isSynchronous = true
-        imageOptions.resizeMode = .exact
-        
-        // Skip the post process images AND the already processed images
-        let startingIndex = batchSize * iter * 2
-        
-        // Ending index is batchSize less then the start, or whatever is left
-        let endingIndex = startingIndex + min(batchSize, imageCount - batchSize * iter)
-        
-        for index in startingIndex ..< endingIndex {
-            let result = results[index]
-            
-            imageManager.requestImage(for: result, targetSize: CGSize(width: CGFloat(4000), height: CGFloat(3000)), contentMode: .aspectFill, options: imageOptions, resultHandler: { (uiImage, info) in
-                loadedImages.append(DroneImage(location: result.location!, image: uiImage!))
-            })
-        }
-        
-        return loadedImages
-    }
-    
-    func deleteOldImages(imageCount: Int) {
-        if imageCount == 0 {
-            return
-        }
-        
-        let options = PHFetchOptions()
-        options.sortDescriptors = [
-            NSSortDescriptor(key:"creationDate", ascending: false)
-        ]
-        options.includeAssetSourceTypes = .typeUserLibrary
-        options.fetchLimit = imageCount * 2
-        options.includeAllBurstAssets = false
-        options.includeHiddenAssets = false
-        
-        let results = PHAsset.fetchAssets(with: .image, options: options)
-        
-        var resultsToDelete = [PHAsset]()
-        
-        for index in imageCount..<imageCount * 2 {
-            resultsToDelete.append(results[index])
-        }
-        
-        let library = PHPhotoLibrary.shared()
-        
-        do {
-            try library.performChangesAndWait {
-                PHAssetChangeRequest.deleteAssets(resultsToDelete as NSArray)
-            }
-        } catch {
-            
-        }
     }
 }
